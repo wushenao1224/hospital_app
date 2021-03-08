d1e4283c5eede93acada681165e2d8760c057e40
WuShenAo <482189837@qq.com>
2021-03-08 13:48:22 +0800

    create hospital

---
+++ components/tab.vue
@@ -0,0 +1,20 @@
+<template>
+	<view>
+		<custom-tab-bar direction="horizontal" :show-icon="false" :selected="selected" @onTabItemTap="onTabItemTap" />
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"tab",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
